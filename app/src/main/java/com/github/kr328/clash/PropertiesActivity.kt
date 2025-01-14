b532cf87edd5676184d17c2bf6f6c23888abb5af
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2025-01-14 21:24:31 +0800

    PropertiesActivity: do not ask if profile not modified

--- app/src/main/java/com/github/kr328/clash/PropertiesActivity.kt
+++ app/src/main/java/com/github/kr328/clash/PropertiesActivity.kt
@@ -16,6 +16,7 @@ import com.github.kr328.clash.design.R
 
 class PropertiesActivity : BaseActivity<PropertiesDesign>() {
     private var canceled: Boolean = false
+    private lateinit var original: Profile
 
     override suspend fun main() {
         setResult(RESULT_CANCELED)
@@ -23,7 +24,7 @@ class PropertiesActivity : BaseActivity<PropertiesDesign>() {
         val uuid = intent.uuid ?: return finish()
         val design = PropertiesDesign(this)
 
-        val original = withProfile { queryByUUID(uuid) } ?: return finish()
+        original = withProfile { queryByUUID(uuid) } ?: return finish()
 
         design.profile = original
 
@@ -72,7 +73,7 @@ class PropertiesActivity : BaseActivity<PropertiesDesign>() {
         design?.apply {
             launch {
                 if (!progressing) {
-                    if (requestExitWithoutSaving())
+                    if (original == profile || requestExitWithoutSaving())
                         finish()
                 }
             }
